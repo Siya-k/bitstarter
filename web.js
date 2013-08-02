55f6236f16b7a546e2d3509caabe4fcc7955b83c
Ubuntu <ubuntu@ip-172-31-27-204.us-west-2.compute.internal>
2013-08-02 10:43:35 +0000

    Edited Title in index.html

--- web.js
+++ web.js
@@ -4,10 +4,11 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send((fs.readFileSync('index.html')).toString());
+var temp = fs.readFileSync('index.html').toString();
+  response.send(temp);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
